bbf05a32349fc75f6988c192348c2a3ab42f61b2
Tugay Demirel <tugaydemirelyedek@gmail.com>
2022-06-29 18:42:23 +0300

    Fix pathname

--- api/index.js
+++ api/index.js
@@ -27,7 +27,7 @@ app.use("/api/users", userRoute);
 app.use("/api/movies", movieRoute);
 app.use("/api/lists", listRoute);
 
-app.use(express.static(path.join(__dirname, "/client")));
+app.use(express.static(path.join(__dirname, "/client/build")));
 
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "/client/build", "index.html"));

947b6e5523c52ca8fc25b8b0902751432e13dc0b
Tugay Demirel <tugaydemirelyedek@gmail.com>
2022-06-29 18:29:51 +0300

    Added port into dotenv

--- api/index.js
+++ api/index.js
@@ -4,17 +4,20 @@ const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const cors = require("cors");
 
-const authRoute = require("./routes/auth")
-const userRoute = require("./routes/users")
-const movieRoute = require("./routes/movies")
-const listRoute = require("./routes/lists")
+const authRoute = require("./routes/auth");
+const userRoute = require("./routes/users");
+const movieRoute = require("./routes/movies");
+const listRoute = require("./routes/lists");
 
-dotenv.config()
+dotenv.config();
 
-mongoose.connect(process.env.MONGO_URL, {
+mongoose
+  .connect(process.env.MONGO_URL, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-}).then(() => console.log("DB Connection succesfull!")).catch((err) => console.log(err))
+  })
+  .then(() => console.log("DB Connection succesfull!"))
+  .catch((err) => console.log(err));
 
 app.use(cors());
 app.use(express.json());
@@ -24,6 +27,6 @@ app.use("/api/users", userRoute);
 app.use("/api/movies", movieRoute);
 app.use("/api/lists", listRoute);
 
-app.listen(8800, () => {
-    console.log("Backend sv is running.")
-})
+app.listen(process.env.PORT || 8800, () => {
+  console.log("Backend sv is running.");
+});
